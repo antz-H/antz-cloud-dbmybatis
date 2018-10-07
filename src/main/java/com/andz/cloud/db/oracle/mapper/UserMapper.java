3cebcff6f25fb90b064807f0806336281520e59e
huanghuang <417213902@qq.com>
2018-10-07 21:42:36 +0800

    1、提交mybaits + tkMapper的动态数据源配置

---
+++ src/main/java/com/andz/cloud/db/oracle/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.andz.cloud.db.oracle.mapper;
+
+import com.andz.cloud.db.oracle.model.User;
+import com.andz.cloud.db.oracle.model.UserExample;
+import com.andz.cloud.db.oracle.utils.MyMapper;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper extends MyMapper<User> {
+
+}
